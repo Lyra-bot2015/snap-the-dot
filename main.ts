9d2455b62912545ae85845b00d971d2719d8244a
Lyra-bot2015 <darknight2k1@hotmail.com>
2024-08-10 11:21:16 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,64 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.B, function () {
     if (Sprite.get(LedSpriteProperty.X) == 2) {
-        game.addScore(1)
+        if (game.score() == 5) {
+            Sprite.delete()
+            game.addScore(1)
+            Cheese += -100
+            basic.showString("LEVEL 2")
+            Sprite = game.createSprite(2, 2)
+        } else {
+            if (game.score() == 10) {
+                Sprite.delete()
+                game.addScore(1)
+                Cheese += -100
+                basic.showString("LEVEL 3")
+                Sprite = game.createSprite(2, 2)
+            } else {
+                if (game.score() == 15) {
+                    Sprite.delete()
+                    game.addScore(1)
+                    Cheese += -100
+                    basic.showString("LEVEL 4")
+                    Sprite = game.createSprite(2, 2)
+                } else {
+                    if (game.score() == 20) {
+                        Sprite.delete()
+                        game.addScore(1)
+                        Cheese += -100
+                        basic.showString("LEVEL 5")
+                        Sprite = game.createSprite(2, 2)
+                    } else {
+                        if (game.score() == 25) {
+                            Sprite.delete()
+                            game.addScore(1)
+                            basic.showString("WINNER!")
+                            music.play(music.stringPlayable("A - F - F A D G ", 120), music.PlaybackMode.UntilDone)
+                            Cheese = 800
+                        } else {
+                            game.addScore(1)
+                        }
+                    }
+                }
+            }
+        }
     } else {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
         music.play(music.tonePlayable(131, music.beat(BeatFraction.Breve)), music.PlaybackMode.UntilDone)
         game.gameOver()
+        basic.clearScreen()
+        Cheese = 800
     }
-})
-input.onButtonPressed(Button.AB, function () {
-    Sprite = game.createSprite(2, 2)
-    Cheese = 200
-    game.setScore(0)
-})
-input.onButtonPressed(Button.B, function () {
-    Cheese += -10
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    Sprite.delete()
-    basic.showIcon(IconNames.Heart)
 })
 let Cheese = 0
 let Sprite: game.LedSprite = null
+basic.showString("LEVEL 1")
 Sprite = game.createSprite(2, 2)
-Cheese = 200
+Cheese = 800
+game.setScore(0)
 basic.forever(function () {
     Sprite.ifOnEdgeBounce()
-    basic.pause(500)
+    basic.pause(Cheese)
 })
 basic.forever(function () {
-    Sprite.move(1)
+    Sprite.change(LedSpriteProperty.X, 1)
     basic.pause(Cheese)
 })
